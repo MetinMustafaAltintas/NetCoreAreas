9244513d9b5e0c29aaba7a515945b911606ca15a
Metin Mustafa Altıntaş <149479684+MetinMustafaAltintas@users.noreply.github.com>
2024-04-10 00:03:00 +0300

    Add project files.

---
+++ NetCoreAreas/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreAreas.Areas.Admin.Controllers
+{
+    //Area'lar Attribute olarak tanımlanmadan calıstırılamaz
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ NetCoreAreas/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NetCoreAreas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
